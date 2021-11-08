c06b92ee50b053be22e4d79f4b2fbd6661131d25
vidyasagar2312 <vidyasagar2312@gmail.com>
2021-11-08 23:26:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SectionComponent } from './section/section.component';
+import { ArticleComponent } from './article/article.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavbarComponent,
+    SectionComponent,
+    ArticleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
